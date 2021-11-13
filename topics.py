d4e4729e0532bb9e535af785648fef076db0e02f
mygeone <miguel.contreras1@mail.udp.cl>
2021-11-13 17:08:33 -0300

    update

--- topics.py
+++ topics.py
@@ -2,8 +2,7 @@ import confluent_kafka.admin, pprint
 def startTopcis():
     conf        = {'bootstrap.servers': 'localhost:9092'}
     kafka_admin = confluent_kafka.admin.AdminClient(conf)
-    new_topic   = confluent_kafka.admin.NewTopic('topic100', 1, 1)
-    new_topic2   = confluent_kafka.admin.NewTopic('topic200', 1, 1)
-
+    new_topic   = confluent_kafka.admin.NewTopic('newOrders', 1, 1)
+    new_topic2   = confluent_kafka.admin.NewTopic('dailySummary', 1, 1)
 
     kafka_admin.create_topics([new_topic,new_topic2]) # CREATE (a list(), so you can create multiple).
